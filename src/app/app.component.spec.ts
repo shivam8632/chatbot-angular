a4aeb60e8c0ec327492881449df5c4babd4df861
developer <developerweb6@gmail.com>
2023-07-06 15:30:58 +0530

    Changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'learning-angular'`, () => {
+  it(`should have as title 'Chatbot'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('learning-angular');
+    expect(app.title).toEqual('Chatbot');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('learning-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Chatbot app is running!');
   });
 });
